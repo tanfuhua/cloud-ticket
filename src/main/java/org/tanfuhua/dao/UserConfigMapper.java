b5a7e9c768c25b4f4b43a7a7af8074d2277d54aa
gaofubo <gao_fubo@163.com>
2023-03-23 21:51:53 +0800

    添加mapper接口

---
+++ src/main/java/org/tanfuhua/dao/UserConfigMapper.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright (C) 2021 Baidu, Inc. All Rights Reserved.
+ */
+package org.tanfuhua.dao;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.tanfuhua.model.entity.UserConfigDO;
+
+/**
+ * @author: gaofubo
+ * @date: 2021/2/20
+ */
+public interface UserConfigMapper extends BaseMapper<UserConfigDO> {
+
+}
