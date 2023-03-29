1ede9e4b928ace5233521552b17ee62296826e00
gaofubo <gao_fubo@163.com>
2023-03-29 11:49:35 +0800

    添加SSL工具类

---
+++ src/main/java/org/tanfuhua/util/SSLUtil.java
@@ -0,0 +1,65 @@
+package org.tanfuhua.util;
+
+import lombok.SneakyThrows;
+import lombok.experimental.UtilityClass;
+import lombok.extern.slf4j.Slf4j;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+
+/**
+ * @author gaofubo
+ * @date 2023/3/26
+ */
+@UtilityClass
+@Slf4j
+public class SSLUtil {
+
+    public static HostnameVerifier createTrustHostnameVerifier() {
+        return (s, sslSession) -> {
+            log.info("HostnameVerifier:host={}SSL信任", s);
+            return true;
+        };
+    }
+
+    @SneakyThrows
+    public static SSLSocketFactory createSSLSocketFactory() {
+        javax.net.ssl.TrustManager[] trustAllCerts = new javax.net.ssl.TrustManager[1];
+        javax.net.ssl.TrustManager tm = new MiTM();
+        trustAllCerts[0] = tm;
+        javax.net.ssl.SSLContext sc = javax.net.ssl.SSLContext.getInstance("SSL", "SunJSSE");
+        sc.init(null, trustAllCerts, null);
+        return sc.getSocketFactory();
+    }
+
+    public static class MiTM implements TrustManager, X509TrustManager {
+
+        public boolean isServerTrusted(X509Certificate[] certs) {
+            return true;
+        }
+
+        public boolean isClientTrusted(X509Certificate[] certs) {
+            return true;
+        }
+
+        @Override
+        public void checkClientTrusted(X509Certificate[] x509Certificates, String s) throws CertificateException {
+
+        }
+
+        @Override
+        public void checkServerTrusted(X509Certificate[] x509Certificates, String s) throws CertificateException {
+
+        }
+
+        @Override
+        public X509Certificate[] getAcceptedIssuers() {
+            return new X509Certificate[0];
+        }
+    }
+
+}
