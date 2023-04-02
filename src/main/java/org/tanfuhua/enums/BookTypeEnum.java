39d900d41ff4808fa64aceff74bca69e071c6631
gaofubo <gao_fubo@163.com>
2023-04-02 15:27:16 +0800

    添加抢订模式枚举

---
+++ src/main/java/org/tanfuhua/enums/BookTypeEnum.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (C) 2021 Baidu, Inc. All Rights Reserved.
+ */
+package org.tanfuhua.enums;
+
+import com.baomidou.mybatisplus.annotation.EnumValue;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+import lombok.Getter;
+
+import java.util.Arrays;
+import java.util.Objects;
+
+/**
+ * 抢订模式
+ *
+ * @author: gaofubo
+ * @date: 2021/3/5
+ */
+@Getter
+public enum BookTypeEnum {
+    INITIAL(0, "初始值"),
+    PERIOD(1, "捡漏抢"),
+    SCHEDULED(2, "预约抢");
+
+    @EnumValue
+    @JsonValue
+    private final int value;
+    private final String desc;
+
+    BookTypeEnum(int value, String desc) {
+        this.value = value;
+        this.desc = desc;
+    }
+
+    @JsonCreator
+    public static BookTypeEnum getEnumByValue(int value) {
+        return Arrays.stream(values()).filter(e -> Objects.equals(e.getValue(), value)).findAny().orElse(null);
+    }
+}
