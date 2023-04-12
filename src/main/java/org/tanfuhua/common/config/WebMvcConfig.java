481d062bcbeb0bbcd6f0d022dc7e0e731db7419e
gaofubo <gao_fubo@163.com>
2023-04-12 10:12:53 +0800

    修改WebMvcConfig

--- src/main/java/org/tanfuhua/common/config/WebMvcConfig.java
+++ src/main/java/org/tanfuhua/common/config/WebMvcConfig.java
@@ -1,6 +1,10 @@
 package org.tanfuhua.common.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
@@ -16,5 +20,20 @@ public class WebMvcConfig extends WebMvcConfigurationSupport {
         //配置拦截器访问静态资源
         registry.addResourceHandler("doc.html").addResourceLocations("classpath:/META-INF/resources/");
         registry.addResourceHandler("/**").addResourceLocations("classpath:/META-INF/resources/");
+    }
+
+    private CorsConfiguration buildConfig() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowCredentials(true);
+        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+        return corsConfiguration;
+    }
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", buildConfig());
+        return new CorsFilter(source);
     }
 }
