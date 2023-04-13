0c787484d9163c26e70ca7db3571644a7450d5d2
gaofubo <gao_fubo@163.com>
2023-04-13 20:39:30 +0800

    修改LowcodeSchemaDO

---
+++ src/main/java/org/tanfuhua/model/entity/LowcodeSchemaDO.java
@@ -0,0 +1,24 @@
+package org.tanfuhua.model.entity;
+
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ * @author gaofubo
+ * @date 2023/4/4
+ */
+@EqualsAndHashCode(callSuper = true)
+@Data
+@TableName("lowcode_schema")
+public class LowcodeSchemaDO extends BaseDO{
+
+    private String name;
+
+    @TableField("`schema`")
+    private String schema;
+
+    private String packages;
+
+}
