008b1c7ba9b967fa6646974fc484d12611d8a6fc
gaofubo <gao_fubo@163.com>
2023-04-16 11:15:42 +0800

    修改UserBO

--- src/main/java/org/tanfuhua/model/bo/UserBO.java
+++ src/main/java/org/tanfuhua/model/bo/UserBO.java
@@ -3,14 +3,17 @@ package org.tanfuhua.model.bo;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
+import java.io.Serializable;
+
 /**
  * @author gaofubo
  * @date 2021/7/25
  */
 @Data
 @EqualsAndHashCode
-public class UserBO {
-
+public class UserBO implements Serializable {
+    private static final long serialVersionUID = 1L;
+    
     private Long id;
 
     private String userName;
