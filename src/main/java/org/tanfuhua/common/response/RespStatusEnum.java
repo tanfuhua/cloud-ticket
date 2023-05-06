dd3a4dbacf4bd62bf6d65e6d0a39c324a2b6d318
gaofubo <gao_fubo@163.com>
2023-05-06 21:33:53 +0800

    添加RespStatusEnum

---
+++ src/main/java/org/tanfuhua/common/response/RespStatusEnum.java
@@ -0,0 +1,22 @@
+package org.tanfuhua.common.response;
+
+import lombok.Getter;
+
+/**
+ * @author gaofubo
+ * @date 2023/5/4
+ */
+@Getter
+public enum RespStatusEnum {
+
+    OK(0, "OK"),
+    ERROR(1000, "ERROR");
+
+    private final int status;
+    private final String msg;
+
+    RespStatusEnum(int status, String msg) {
+        this.status = status;
+        this.msg = msg;
+    }
+}
