3426f0c5839ac91ab30500cb7711298c7d6e5825
gaofubo <gao_fubo@163.com>
2023-05-10 09:44:07 +0800

    添加响应状态枚举

---
+++ src/main/java/org/tanfuhua/dao/LowcodeScenarioMapper.java
@@ -0,0 +1,12 @@
+package org.tanfuhua.dao;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.tanfuhua.model.entity.LowcodeScenarioDO;
+
+/**
+ * @author: gaofubo
+ * @date: 2021/2/20
+ */
+public interface LowcodeScenarioMapper extends BaseMapper<LowcodeScenarioDO> {
+
+}
